fe4ff5798a728107577256c4f0c53ae3db459b40
Josue Canales <josuecanales377@gmail.com>
2024-02-09 11:29:04 -0600

    "Practica Tab"

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -5,36 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tempVal;
     Button btn;
-
+    RadioGroup op;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn=findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tempVal=findViewById(R.id.txtnum1);
-                double num1=Double.parseDouble(tempVal.getText().toString());
-
-                tempVal=findViewById(R.id.txtnum2);
-                double num2=Double.parseDouble(tempVal.getText().toString());
-
-                double resultado= num1+num2;
-                tempVal=findViewById(R.id.lblresultado);
-                tempVal.setText("Resultado = "+resultado);
-            }
-        });
-
-
 
     }
 }
