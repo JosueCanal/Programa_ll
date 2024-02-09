122130fdd9d69537dd8b85170c49f97236b3e440
Josue Canales <josuecanales377@gmail.com>
2024-02-09 11:29:04 -0600

    "Practica 3"

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tempVal;
     Button btn;
-
+    RadioGroup op;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +29,38 @@ public class MainActivity extends AppCompatActivity {
                 tempVal=findViewById(R.id.txtnum2);
                 double num2=Double.parseDouble(tempVal.getText().toString());
 
-                double resultado= num1+num2;
+                double resultado=0;
+                op=findViewById(R.id.optOperadores);
+                switch (op.getCheckedRadioButtonId()){
+                    case R.id.opSuma:
+                        resultado = num1+num2;
+                        break;
+                    case R.id.opResta:
+                        resultado = num1-num2;
+                        break;
+                    case R.id.opMulplicacion:
+                        resultado = num1*num2;
+                        break;
+                    case R.id.opDivision:
+                        resultado = num1/num2;
+                        break;
+                    case R.id.opPorcentaje:
+                        resultado = num1 * (num2/100);
+                        break;
+                    case R.id.opExponente:
+                        resultado = Math.pow(num1, num2);
+                        break;
+                    case R.id.opFactorial:
+                        long factorial = 1;
+                        for (int i = 1; i <= num1; i++) {
+                            factorial *= i;
+                            resultado = factorial;
+                        }
+                        break;
+                    case R.id.opRaiz:
+                        resultado = Math.pow(num1, 1/num2);
+                        break;
+                }
                 tempVal=findViewById(R.id.lblresultado);
                 tempVal.setText("Resultado = "+resultado);
             }
