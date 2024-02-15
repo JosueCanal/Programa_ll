d9a3b0c60c4742ba6f28b7ec2de244c8ac5b157a
Josue Canales <josuecanales377@gmail.com>
2024-02-15 17:53:03 -0600

    "Practica Tab"

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,18 +6,53 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tempVal;
     Button btn;
-    RadioGroup op;
-
+    Spinner spn;
+    TabHost tbh;
+    conversores miObj = new conversores();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    tbh=findViewById(R.id.tbhConversores);
+    tbh.setup();
+    tbh.addTab(tbh.newTabSpec("LON").setContent(R.id.tabLongitud).setIndicator("LONGITUD",null));
+    tbh.addTab(tbh.newTabSpec("ALM").setContent(R.id.tabLongitud).setIndicator("ALMACENAMIENTO",null));
+    tbh.addTab(tbh.newTabSpec("MON").setContent(R.id.tabLongitud).setIndicator("MONEDAS",null));
+    btn=findViewById(R.id.btnConvertirLongitud);
+    btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            spn=findViewById(R.id.spnDeLongitud);
+            int de=spn.getSelectedItemPosition();
+
+            spn=findViewById(R.id.spnALongitud);
+            int a=spn.getSelectedItemPosition();
+
+            tempVal=findViewById(R.id.txtCantidadLongitud);
+            double cantidad= Double.parseDouble(tempVal.getText().toString());
+
+            double resultado= miObj.convertir(0,de,a,cantidad);
+            Toast.makeText(getApplicationContext(),"Resultado: "+resultado,Toast.LENGTH_LONG).show();
+        }
+    });
+    }
+}
+class conversores{
+    double[][] valores={
+            {1,100,39.3701,3.28084, 1.193, 1.09361, 0.001, 0.000621371},
+            {}
+    };
+    public double convertir(int opcion,int de,int a,double cantidad){
+        return valores[opcion][a]/valores[opcion][de]*cantidad;
     }
 }
